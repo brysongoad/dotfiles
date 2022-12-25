5bc9640effe48194fd8a827f0f76e1762a494d96
Bryson Goad <dev@brysongoad.com>
2022-12-25 12:52:22 -0600

    remove local-env.bash

--- bash/.bash/local-env.bash
+++
@@ -1,7 +0,0 @@
-#
-# Local environment not to be included in git
-#
-
-export SASS_PATH='./node_modules'
-export EVENTIR_DATABASE_USER_PASSWORD=system
-export EVENTIR_OPENID_CLIENT_SECRET=qxVIyMgeZquvs5aSSCAvZpCQ
