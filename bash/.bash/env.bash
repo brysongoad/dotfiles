fa461b5878d260f8ccdb0af5f4decc8025cdeed2
Bryson Goad <dev@brysongoad.com>
2023-01-20 14:33:58 -0600

    remove local environment setup from env.bash

--- bash/.bash/env.bash
+++ bash/.bash/env.bash
@@ -1,9 +1,6 @@
 #
 # Bash environment
 #
-export GEM_HOME="$HOME/.gem"
-export PATH="$HOME/.gem/ruby/2.6.0/bin:$PATH"
 export HISTCONTROL=ignoredups # Ignore duplicate history entries
-
 export EDITOR=nvim
 
